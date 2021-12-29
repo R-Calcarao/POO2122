f5ec057b0e78ed5046d928e757ea133029ee797b
Fl13ip <94529914+Fl13ip@users.noreply.github.com>
2021-12-29 15:43:22 +0000

    Update Edificio.h

--- Edificio.h
+++ Edificio.h
@@ -1,22 +1,54 @@
-//
-// Created by ACER on 23/12/2021.
-//
-
 #ifndef PRATICOTRABALHO_EDIFICIO_H
 #define PRATICOTRABALHO_EDIFICIO_H
 
 #include <string>
+#include <sstream>
 
 using namespace std;
+
+class Recursos;
+class Zona;
 
 class Edificio {
 
     const string tipo;
     int lvlPrice;
     int capMax;
-public:
-    Edificio(const string tipo, const int lvlPrice, int capMax) : tipo(tipo),lvlPrice(lvlPrice),capMax(capMax){}
+    int construcDay;
+    Recursos* recursos;
+    Zona* zona;
+    bool ligado=false;
 
+
+public:
+    Edificio(const string tipo, const int lvlPrice, int capMax, int construcDay) : tipo(tipo),lvlPrice(lvlPrice),
+                                                                                   capMax(capMax), construcDay(construcDay){}
+
+    void pointToRecursos(Recursos* abc){
+        recursos = abc;
+    }
+
+    string getTipo() const{
+        return tipo;
+    }
+
+    int getConstrucDay() const{
+        return construcDay;
+    }
+
+    void pointToZona(Zona* abc){
+        zona = abc;
+    }
+
+    Zona* getZonaRef() const{
+        return zona;
+    }
+
+    string flag();
+
+     virtual void newDay(){}
+
+     virtual void produzir(){}
 
 };
 
