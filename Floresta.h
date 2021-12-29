ac1f182b0575ffbee3a6de3acabcc43569485048
Fl13ip <94529914+Fl13ip@users.noreply.github.com>
2021-12-29 15:44:17 +0000

    Update Floresta.h

--- Floresta.h
+++ Floresta.h
@@ -1,7 +1,3 @@
-//
-// Created by ACER on 23/12/2021.
-//
-
 #ifndef PRATICOTRABALHO_FLORESTA_H
 #define PRATICOTRABALHO_FLORESTA_H
 
@@ -12,6 +8,7 @@ class Floresta : public Zona {
     const string tipo;
     int N_arvores;
     int Madeira=0;//É produzida por Lenhadores
+    int Cap_Max=100;
 public:
 
     Floresta() : Zona("flr"){
@@ -21,6 +18,29 @@ public:
 
     int getNumArvores() const{
         return N_arvores;
+    }
+
+    void newDay() override {
+        Zona::newDay();
+        if(getDay()%2 == 0 && N_arvores < Cap_Max && getNumEdificios() == 0) {//Só crescem arvores se n existirem edi.
+            N_arvores++;
+        } else { //Se existirem edificios morre uma arvore por cada dia que passa
+            N_arvores--;
+        }
+        if(N_arvores > Cap_Max){ //Só para ter certeza que o limite n arvores n é passado
+            while(N_arvores > Cap_Max) {
+                N_arvores--;
+            }
+        }
+        addMadeira(getNumbLen()*1);
+    }
+
+    string getAsString() override{
+        ostringstream oss;
+        oss << Zona::getAsString();
+        oss << "Numero de Arvores: " << getNumArvores() << endl;
+        oss << endl;
+        return oss.str();
     }
 
 };
