d356952705d5e94db7470fbab26f21363010ab39
Fl13ip <94529914+Fl13ip@users.noreply.github.com>
2021-12-29 15:49:15 +0000

    Update MinaFerro.cpp

--- MinaFerro.cpp
+++ MinaFerro.cpp
@@ -1,5 +1,25 @@
-//
-// Created by ACER on 23/12/2021.
-//
-
 #include "MinaFerro.h"
+#include "Zona.h"
+
+void MinaFerro::newDay() {
+    Edificio::newDay();
+    bool TrueFalse = (rand() % 100) < ProbCollap;
+    if(TrueFalse){
+        cout << "O edificio " << getTipo() << " desabou na Zona " << getZonaRef()->getTipoZona() << endl;
+        getZonaRef()->removeEdificio();
+    }else if(getZonaRef()->getDay() - getConstrucDay() == 10 && getZonaRef()->getTipoZona() == "pnt") {
+        cout << "O edificio " << getTipo() << " afundou na Zona " << getZonaRef()->getTipoZona() << endl;
+        getZonaRef()->removeEdificio();
+        getZonaRef()->fireAllWorkers();
+    }
+}
+
+void MinaFerro::produzir() {
+    Edificio::produzir();
+    if(getZonaRef()->haveMiner() && getZonaRef()->getTipoZona() == "mnt") {
+        getZonaRef()->addFerro(4);
+    }
+    if(getZonaRef()->haveMiner()) {
+        getZonaRef()->addFerro(2);
+    }
+}
