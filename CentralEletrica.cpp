acab64bce0448a814183e4c39889efbd8490ffa6
Fl13ip <94529914+Fl13ip@users.noreply.github.com>
2022-01-02 17:29:18 +0000

    Update CentralEletrica.cpp

--- CentralEletrica.cpp
+++ CentralEletrica.cpp
@@ -1,3 +1,5 @@
+
+
 #include "CentralEletrica.h"
 #include "Zona.h"
 
@@ -7,5 +9,32 @@ void CentralEletrica::newDay() {
         cout << "O edificio " << getTipo() << " afundou na Zona " << getZonaRef()->getTipoZona() << endl;
         getZonaRef()->removeEdificio();
         getZonaRef()->fireAllWorkers();
+    }
+}
+
+void CentralEletrica::produzir() {
+    Edificio::produzir();
+    if(getZonaRef()->haveOper()
+    && getZonaRef()->getTipoZona() == "mnt"
+    && getZonaRef()->getMadeira() >= 1
+    && getZonaRef()->HaveflrAround()) {
+
+        getZonaRef()->withdrawMadeira(1);
+        getZonaRef()->addCarvao(2);
+
+    } else if(getZonaRef()->haveOper()
+              && getZonaRef()->getMadeira() >= 1
+              && getZonaRef()->HaveflrAround()) {
+
+        getZonaRef()->withdrawMadeira(1);
+        getZonaRef()->addCarvao(1);
+    } else {
+        if(!getZonaRef()->haveOper()) {
+            cout << "A Central Eletrica nao tem nenhum operario a trabalhar." << endl;
+        } else if(getZonaRef()->getMadeira() < 1) {
+            cout << "A Central Eletrica nao tem materia suficiente." << endl;
+        } else if(!getZonaRef()->HaveflrAround()) {
+            cout << "A Centrel nao esta adjacente a nenhuma floresta ou a floresta nao produziu madeira." << endl;
+        }
     }
 }
