09a5ca89dbe19652b0a2bd2beb972df31906bf2c
Fl13ip <94529914+Fl13ip@users.noreply.github.com>
2022-01-02 17:37:24 +0000

    Update Lenhador.h

--- Lenhador.h
+++ Lenhador.h
@@ -1,6 +1,3 @@
-//
-// Created by ACER on 23/12/2021.
-//
 
 #ifndef PRATICOTRABALHO_LENHADOR_H
 #define PRATICOTRABALHO_LENHADOR_H
@@ -9,12 +6,19 @@
 
 class Lenhador : public Trabalhadores{
 
-    int dayCount=1;
+    int dayCount=0;
     bool rest=false;
+    bool aux = false;
+
 public:
+
     Lenhador(const int dia) : Trabalhadores("len",20,dia){}
 
     void newWorkDay() override;
+
+    bool Resting() override{
+        return rest;
+    }
 
 };
 
