97df957a71c85e0dc5e5cab129310d9ff7c1bd55
Fl13ip <94529914+Fl13ip@users.noreply.github.com>
2022-01-02 17:47:09 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 int main() {
 
+   // cout << RED << "hello world" << RESET << endl;
+
     Ilha I(5,5);
 
     int linha;
@@ -20,11 +22,13 @@ int main() {
 
 
 
-    while(1){
+   while(1){
         cout << "1 - Contratar Worker. (oper/len/miner)" << endl;
         cout << "2 - Mover Worker." << endl;
         cout << "3 - Listar Zona." << endl;
-        cout << "4 - New Day." << endl;
+        cout << "4 - Construir Edificio. (mnC/mnF/bat/fun)" << endl;
+        cout << "5 - New Day." << endl;
+        cout << "6 - Produzir." << endl;
         cout << ">>  ";
         scanf("%d",&option);
 
@@ -59,7 +63,23 @@ int main() {
                 break;
 
             case 4:
+                cout << "Tipo: ";
+                cin >> tipo;
+                cout << "Linha: ";
+                scanf("%d", &linha);
+                cout << "Coluna: ";
+                scanf("%d", &coluna);
+                I.addEdi(tipo,linha,coluna);
+                I.showIlha();
+                break;
+
+            case 5:
                 I.newDay();
+                I.showIlha();
+                break;
+
+            case 6:
+                I.produzir();
                 I.showIlha();
                 break;
 
