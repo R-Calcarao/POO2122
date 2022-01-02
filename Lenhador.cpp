fab8e62071ea8252d4feeae24cf9753af72f1a55
Fl13ip <94529914+Fl13ip@users.noreply.github.com>
2022-01-02 17:36:50 +0000

    Update Lenhador.cpp

--- Lenhador.cpp
+++ Lenhador.cpp
@@ -1,11 +1,25 @@
-//
-// Created by ACER on 23/12/2021.
-//
 
 #include "Lenhador.h"
+#include "Zona.h"
 
 void Lenhador::newWorkDay() {
-    //ainda n sei como fazer
-
-
+    Trabalhadores::newWorkDay();
+    dayCount++;
+    if(giveZonaRef()->getTipoZona() != "mnt") { //Se nao estiver na montanha
+        if(aux){
+            rest = false; // Pronto para trabalhar +4 dias
+            dayCount = 0; // counter é resetado
+            aux = false;
+        } else if(dayCount > 4) {
+            rest = true; // Nao esta disponivel para trabalhar hoje
+            aux = true; // flag ativada
+        }
+    } else { //Se tiver numa Montanha
+        bool TrueFalse = (rand() % 100) < 5;
+        if(TrueFalse){
+            cout << "Vou me embora da zona: " << giveZonaRef()->getTipoZona() << ", sou o " << getTipo() << " de id: " <<
+                 getId() << " . Adeus!!" << endl;
+            giveZonaRef()->fireWorker(getId());
+        }
+    }
 }
